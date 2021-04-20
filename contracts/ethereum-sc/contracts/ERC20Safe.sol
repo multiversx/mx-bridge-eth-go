ede0c90866ede512c08dce0fa95638cfe78c8a71
Adrian Nistor <nistor.adrian@agilefreaks.com>
2021-04-20 12:59:32 +0300

    Add whitelisted tokens on ERC20Safe

--- contracts/ethereum-sc/contracts/ERC20Safe.sol
+++ contracts/ethereum-sc/contracts/ERC20Safe.sol
@@ -2,46 +2,71 @@
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/access/AccessControl.sol";
 
-// TODO: Rename to ERC20Bridge
-contract ERC20Safe {
+contract ERC20Safe is AccessControl {
+    enum DepositStatus {Pending, InProgress, Executed, Rejected}
+
     struct Deposit {
         address tokenAddress;
         uint256 amount;
         address depositor;
         bytes recipient;
+        DepositStatus status;
     }
 
-    event ERC20Deposited(    
-        uint64 depositIndex
-    );
-
+    // STATE
     uint64 public depositsCount;
+    mapping(uint64 => Deposit) public _deposits;
+    mapping(address => bool) public _whitelistedTokens;
 
-    mapping(uint64 => Deposit) deposits;
-  
-  /**
+    // EVENTS
+    event ERC20Deposited(uint64 depositIndex);
+
+    constructor() {
+        _setupRole(DEFAULT_ADMIN_ROLE, msg.sender);
+    }
+
+    function whitelistToken(address token) public {
+        _whitelistedTokens[token] = true;
+    }
+
+    /**
 
       @notice It assumes that tokenAddress is a corect address for an ERC20 token. No checks whatsoever for this (yet)
       @param tokenAddress Address of the contract for the ERC20 token that will be deposited
       @param amount number of tokens that need to be deposited
-      @param recipientAddress address of the r eceiver of tokens on Elrond Network
+      @param recipientAddress address of the receiver of tokens on Elrond Network
       @notice emits {ERC20Deposited} event
    */
-    function deposit(address tokenAddress, uint256 amount, bytes calldata recipientAddress) public {        
+    function deposit(
+        address tokenAddress,
+        uint256 amount,
+        bytes calldata recipientAddress
+    ) public {
+        require(_whitelistedTokens[tokenAddress], "Unsupported token");
         uint64 depositIndex = ++depositsCount;
-        deposits[depositIndex] = Deposit(tokenAddress, amount, msg.sender, recipientAddress);
-        
-        lockTokens(tokenAddress, msg.sender, amount);
+        _deposits[depositIndex] = Deposit(
+            tokenAddress,
+            amount,
+            msg.sender,
+            recipientAddress,
+            DepositStatus.Pending
+        );
+
+        // lockTokens(tokenAddress, amount, msg.sender);
         emit ERC20Deposited(depositIndex);
     }
 
-    function lockTokens(address tokenAddress, uint256 amount, address owner) internal {
+    function lockTokens(
+        address tokenAddress,
+        uint256 amount,
+        address owner
+    ) internal {
         IERC20 erc20 = IERC20(tokenAddress);
         _safeTransferFrom(erc20, owner, address(this), amount);
     }
-`
- 
+
     /**
         @notice Gets information about a deposit into the bridge
         @param depositIndex Index of the deposit. Also represents the n-th deposit that was made
@@ -51,24 +76,40 @@ contract ERC20Safe {
         - depositor Address of the account that deposited the tokens
         - recipient Address where tokens will be minted on Elrond Network
     */
-    function getDeposit(uint64 depositIndex) external view returns (Deposit memory) {
-        return deposits[depositIndex];
+    function getDeposit(uint64 depositIndex)
+        external
+        view
+        returns (Deposit memory)
+    {
+        return _deposits[depositIndex];
     }
 
     // function _safeTransfer(IERC20 token, address to, uint256 value) private {
     //     _safeCall(token, abi.encodeWithSelector(token.transfer.selector, to, value));
     // }
-    
-    // function _safeTransferFrom(IERC20 token, address from, address to, uint256 value) private {
-    //     _safeCall(token, abi.encodeWithSelector(token.transferFrom.selector, from, to, value));
-    // }
 
-    // function _safeCall(IERC20 token, bytes memory data) private {        
-    //     (bool success, bytes memory returndata) = address(token).delegatecall(data);
-    //     require(success, "ERC20: call failed");
+    function _safeTransferFrom(
+        IERC20 token,
+        address from,
+        address to,
+        uint256 value
+    ) private {
+        _safeCall(
+            token,
+            abi.encodeWithSelector(token.transferFrom.selector, from, to, value)
+        );
+    }
 
-    //     if (returndata.length > 0) {
-    //         require(abi.decode(returndata, (bool)), "ERC20: operation did not succeed");
-    //     }
-    // }
+    function _safeCall(IERC20 token, bytes memory data) private {
+        (bool success, bytes memory returndata) =
+            address(token).delegatecall(data);
+        require(success, "ERC20: call failed");
+
+        if (returndata.length > 0) {
+            require(
+                abi.decode(returndata, (bool)),
+                "ERC20: operation did not succeed"
+            );
+        }
+    }
 }
--- contracts/ethereum-sc/test/ERC20Safe-test.js
+++ contracts/ethereum-sc/test/ERC20Safe-test.js
@@ -1,24 +1,72 @@
+
 const { expect } = require("chai");
-const { deployMockContract, MockProvider, solidity, deployContract } = require('ethereum-waffle');
+const { waffle } = require("hardhat");
+const { deployMockContract, provider, deployContract } = waffle;
+
 const IERC20 = require('../artifacts/@openzeppelin/contracts/token/ERC20/IERC20.sol/IERC20.json');
 const ERC20Safe = require('../artifacts/contracts/ERC20Safe.sol/ERC20Safe.json');
-describe("ERC20Safe", function() {
-  describe('depositERC20', function() {
-    let token;
-    let contract;
-    const [wallet, otherWallet] = new MockProvider().getWallets();
 
-    beforeEach(async function() {
-      token = await deployMockContract(wallet, IERC20.abi);
-      contract = await deployContract(wallet, ERC20Safe);
+describe("ERC20Safe", async function () {
+  const [adminWallet, otherWallet] = provider.getWallets();
+
+  beforeEach(async function () {
+    token = await deployMockContract(adminWallet, IERC20.abi);
+    safe = await deployContract(adminWallet, ERC20Safe);
+  });
+
+  it('sets creator as admin', async function () {
+    ADMIN_ROLE = await safe.DEFAULT_ADMIN_ROLE();
+    expect(await safe.hasRole(ADMIN_ROLE, adminWallet.address)).to.be.true;
+  });
+
+  describe('whitelistToken', async function () {
+    it('adds the token to the whitelistedTokens list', async function () {
+      await safe.whitelistToken(token.address);
+
+      expect(await safe._whitelistedTokens(token.address)).to.be.true;
+    })
+  });
+
+  describe('deposit', async function () {
+    let amount = 100;
+
+    describe("when token is whitelisted", async function () {
+      beforeEach(async function () {
+        await safe.whitelistToken(token.address);
+      })
+
+      it("emits Deposited event", async () => {
+        // await token.mock.transferFrom.returns({});
+        await expect(safe.deposit(token.address, amount, ethers.utils.toUtf8Bytes("some address")))
+          .to.emit(safe, "ERC20Deposited")
+          .withArgs(1);
+      });
+
+      it('increments depositsCount', async () => {
+        await safe.deposit(token.address, amount, ethers.utils.toUtf8Bytes("some address"));
+
+        expect(await safe.depositsCount.call()).to.equal(1);
+      });
+
+      it('creates a deposit', async function () {
+        await safe.deposit(token.address, amount, ethers.utils.toUtf8Bytes("some address"));
+
+        deposit = await safe.getDeposit(1);
+
+        expect(deposit.tokenAddress).to.equal(token.address);
+        expect(deposit.amount).to.equal(amount);
+        expect(deposit.depositor).to.equal(adminWallet.address);
+        expect(deposit.status).to.equal(0/*pending*/);
+        expect(ethers.utils.toUtf8String(deposit.recipient)).to.equal("some address");
+      });
     });
 
-    it("Emits Deposited event", async () => {
-      let amount = 100;
-      await token.mock.transferFrom.returns({});
-      await expect(contract.depositERC20(token.address, amount))
-        .to.emit(contract, "ERC20Deposited")
-        .withArgs(token.address, wallet.address, amount)
+
+    describe("when token is not whitelisted", async function () {
+      it('reverts', async function() {
+        await expect(safe.deposit(token.address, amount, ethers.utils.toUtf8Bytes("some address")))
+        .to.be.revertedWith('Unsupported token');
+      })
     });
   });
 });
