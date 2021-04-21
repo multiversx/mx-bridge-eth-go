5b3320406df7e78fc80b10276800332df8404af7
Adrian Nistor <nistor.adrian@agilefreaks.com>
2021-04-21 19:57:11 +0300

    Update deploy script

--- contracts/ethereum-sc/scripts/deploy.js
+++ contracts/ethereum-sc/scripts/deploy.js
@@ -14,19 +14,43 @@ async function main() {
   // await hre.run('compile');
 
   // We get the contract to deploy
+  [adminWallet, relayer1, relayer2, relayer3, relayer4, relayer5, depositor] = await hre.ethers.getSigners();
+
+  console.log('Admin Public Address:', adminWallet.address);
+  console.log('Relayer 1 Public Address:', relayer1.address);
+  console.log('Relayer 2 Public Address:', relayer2.address);
+  console.log('Relayer 3 Public Address:', relayer3.address);
+  console.log('Relayer 4 Public Address:', relayer4.address);
+  console.log('Relayer 5 Public Address:', relayer5.address);
+  console.log('Depositor Public Address:', depositor.address);
+
+  // Deploy ERC20 tokens
   const AFC = await hre.ethers.getContractFactory("AFCoin");
-  const AFCContract = await AFC.deploy(10);
-
+  const AFCContract = await AFC.deploy(100);
   await AFCContract.deployed();
-
   console.log("AFCContract deployed to:", AFCContract.address);
 
+  depositorAFC = AFCContract.connect(depositor);
+  await depositorAFC.brrr();
+  console.log("Depositor created AFC: ", await depositorAFC.balanceOf(depositor.address));
+
+  // Deploy ERC20 Safe
   const ERC20Safe = await hre.ethers.getContractFactory("ERC20Safe");
   const safeContract = await ERC20Safe.deploy();
-
   await safeContract.deployed();
-
   console.log("ERC20Safe deployed to:", safeContract.address);
+
+  // Whitelist ERC20 tokens in the ERC20 Safe
+  await safeContract.whitelistToken(AFCContract.address);
+
+  // Deploy Bridge with ERC20 Safe address
+  const Bridge = await hre.ethers.getContractFactory("Bridge");
+  const bridgeContract = await Bridge.deploy([adminWallet.address, relayer1.address, relayer2.address, relayer3.address, relayer4.address, relayer5.address], 4, safeContract.address);
+  await bridgeContract.deployed();
+  console.log("Bridge deployed to:", bridgeContract.address);
+
+  // Finish setup of ERC20 Safe with the Bridge so onlyBridge modifiers can be successful
+  await safeContract.setBridgeAddress(bridgeContract.address);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
