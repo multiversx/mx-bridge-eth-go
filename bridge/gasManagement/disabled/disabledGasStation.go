79db181084bf4e62b3ab09a860c3d68f4473a553
Rebegea Dragos-Alexandru <dragosalex97@gmail.com>
2021-10-29 12:55:49 +0300

    commits for gasStation toggle

--- bridge/gasManagement/disabled/disabledGasStation.go
+++ bridge/gasManagement/disabled/disabledGasStation.go
@@ -2,8 +2,10 @@ package disabled
 
 import "math/big"
 
+// DisabledGasStation implementation in case no gasStation is used
 type DisabledGasStation struct{}
 
+// GetCurrentGasPrice return nil; gas price will be automatically determined
 func (dgs *DisabledGasStation) GetCurrentGasPrice() (*big.Int, error) {
 	return nil, nil
 }
--- bridge/gasManagement/factory/gasManagementFactory.go
+++ bridge/gasManagement/factory/gasManagementFactory.go
@@ -6,6 +6,7 @@ import (
 	"github.com/ElrondNetwork/elrond-eth-bridge/bridge/gasManagement/disabled"
 )
 
+// CreateGasStation generates an implementation of GasHandler
 func CreateGasStation(args gasManagement.ArgsGasStation, enabled bool) (bridge.GasHandler, error) {
 	if enabled {
 		return gasManagement.NewGasStation(args)
