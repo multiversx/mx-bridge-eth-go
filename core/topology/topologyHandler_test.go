c196aaf1fc8e932b591fe60affce4ce7b5a1c015
Sorin Stanculeanu <sorinstanculeanu26@gmail.com>
2021-12-09 10:11:13 +0200

    Fixed mock for args only on tests

--- core/topology/topologyHandler_test.go
+++ core/topology/topologyHandler_test.go
@@ -4,7 +4,6 @@ import (
     "testing"
     "time"
 
-    "github.com/ElrondNetwork/elrond-eth-bridge/core"
     "github.com/ElrondNetwork/elrond-eth-bridge/testsCommon"
     "github.com/stretchr/testify/assert"
 )
@@ -15,47 +14,53 @@ func TestNewTopologyHandler(t *testing.T) {
     t.Parallel()
 
     t.Run("should work", func(t *testing.T) {
-        providedSortedPublicKeys := [][]byte{[]byte("aaa"), []byte("bbb")}
-        providedTimer := createTimerStubWithUnixValue(123)
-        providedAddress := []byte("aaa")
-        providedStepDuration := duration
-        tph, err := createTopologyHandler(providedSortedPublicKeys, providedTimer, providedStepDuration, providedAddress)
+        args := createMockArgsTopologyHandler()
+        tph, err := NewTopologyHandler(args)
+
         assert.NotNil(t, tph) // pointer check
         assert.Nil(t, err)
         assert.False(t, tph.IsInterfaceNil()) // IsInterfaceNIl
 
-        assert.Equal(t, providedSortedPublicKeys, tph.sortedPublicKeys)
-        assert.Equal(t, providedTimer, tph.timer)
-        assert.Equal(t, providedAddress, tph.address)
-        assert.Equal(t, providedStepDuration, tph.stepDuration)
+        assert.Equal(t, args.SortedPublicKeys, tph.sortedPublicKeys)
+        assert.Equal(t, args.Timer, tph.timer)
+        assert.Equal(t, args.StepDuration, tph.stepDuration)
+        assert.Equal(t, args.Address, tph.address)
     })
 
     t.Run("nil providedSortedPublicKeys", func(t *testing.T) {
-        expectedErr := errNilSortedPublicKeys
-        tph, err := createTopologyHandler(nil, nil, 0, nil)
+        args := createMockArgsTopologyHandler()
+        args.SortedPublicKeys = nil
+        tph, err := NewTopologyHandler(args)
+
         assert.Nil(t, tph)
-        assert.Equal(t, expectedErr, err)
+        assert.Equal(t, errNilSortedPublicKeys, err)
     })
 
     t.Run("nil timer", func(t *testing.T) {
-        expectedErr := errNilTimer
-        tph, err := createTopologyHandler([][]byte{[]byte("abc")}, nil, 0, nil)
+        args := createMockArgsTopologyHandler()
+        args.Timer = nil
+        tph, err := NewTopologyHandler(args)
+
         assert.Nil(t, tph)
-        assert.Equal(t, expectedErr, err)
+        assert.Equal(t, errNilTimer, err)
     })
 
     t.Run("invalid step duration", func(t *testing.T) {
-        expectedErr := errInvalidStepDuration
-        tph, err := createTopologyHandler([][]byte{[]byte("abc")}, createTimerStubWithUnixValue(0), time.Duration(12345), nil)
+        args := createMockArgsTopologyHandler()
+        args.StepDuration = time.Duration(12345)
+        tph, err := NewTopologyHandler(args)
+
         assert.Nil(t, tph)
-        assert.Equal(t, expectedErr, err)
+        assert.Equal(t, errInvalidStepDuration, err)
     })
 
     t.Run("nil address", func(t *testing.T) {
-        expectedErr := errNilAddress
-        tph, err := createTopologyHandler([][]byte{[]byte("abc")}, createTimerStubWithUnixValue(0), duration, nil)
+        args := createMockArgsTopologyHandler()
+        args.Address = nil
+        tph, err := NewTopologyHandler(args)
+
         assert.Nil(t, tph)
-        assert.Equal(t, expectedErr, err)
+        assert.Equal(t, errNilAddress, err)
     })
 }
 
@@ -63,20 +68,25 @@ func TestMyTurnAsLeader(t *testing.T) {
     t.Parallel()
 
     t.Run("not leader - SortedPublicKeys empty", func(t *testing.T) {
-        tph, _ := createTopologyHandler([][]byte{}, createTimerStubWithUnixValue(0), duration, []byte("abc"))
+        args := createMockArgsTopologyHandler()
+        args.SortedPublicKeys = [][]byte{}
+        tph, _ := NewTopologyHandler(args)
+
         assert.False(t, tph.MyTurnAsLeader())
     })
 
     t.Run("not leader", func(t *testing.T) {
-        tph, _ := createTopologyHandler([][]byte{[]byte("aaa"), []byte("bbb")}, createTimerStubWithUnixValue(0), duration, []byte("abc"))
+        args := createMockArgsTopologyHandler()
+        args.Address = []byte("abc")
+        tph, _ := NewTopologyHandler(args)
 
         // 0/1%2=0 -> providedSortedPublicKeys[0] != providedAddress -> not leader
         assert.False(t, tph.MyTurnAsLeader())
     })
 
     t.Run("leader", func(t *testing.T) {
-        expectedLeader := []byte("aaa")
-        tph, _ := createTopologyHandler([][]byte{expectedLeader, []byte("bbb")}, createTimerStubWithUnixValue(0), duration, expectedLeader)
+        args := createMockArgsTopologyHandler()
+        tph, _ := NewTopologyHandler(args)
 
         // index=0/1%2=0 -> providedSortedPublicKeys[0] == providedAddress -> leader
         assert.True(t, tph.MyTurnAsLeader())
@@ -91,12 +101,11 @@ func createTimerStubWithUnixValue(value int64) *testsCommon.TimerStub {
     return stub
 }
 
-func createTopologyHandler(pk [][]byte, timer core.Timer, duration time.Duration, address []byte) (*topologyHandler, error) {
-    args := ArgsTopologyHandler{
-        SortedPublicKeys: pk,
-        Timer:            timer,
+func createMockArgsTopologyHandler() ArgsTopologyHandler {
+    return ArgsTopologyHandler{
+        SortedPublicKeys: [][]byte{[]byte("aaa"), []byte("bbb")},
+        Timer:            createTimerStubWithUnixValue(0),
         StepDuration:     duration,
-        Address:          address,
+        Address:          []byte("aaa"),
     }
-    return NewTopologyHandler(args)
 }
