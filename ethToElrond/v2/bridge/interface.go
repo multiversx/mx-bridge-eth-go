13c2961545aa38e10dc15b515d2535ada24548a4
Sorin Stanculeanu <sorinstanculeanu26@gmail.com>
2021-12-15 18:11:59 +0200

    add missing interface.go

---
+++ ethToElrond/v2/bridge/interface.go
@@ -0,0 +1,55 @@
+package bridge
+
+import (
+	"context"
+
+	"github.com/ElrondNetwork/elrond-eth-bridge/clients"
+	logger "github.com/ElrondNetwork/elrond-go-logger"
+)
+
+// Bridge defines a generic bridge interface able to handle both halves of the bridge
+type Bridge interface {
+	GetLogger() logger.Logger
+
+	MyTurnAsLeaderOnElrond() bool
+	GetBatchFromElrond(ctx context.Context) (*clients.TransferBatch, error)
+	StoreBatchFromElrond(batch *clients.TransferBatch) error
+	GetStoredBatch() *clients.TransferBatch
+	GetLastExecutedEthBatchIDFromElrond(ctx context.Context) (uint64, error)
+	VerifyLastDepositNonceExecutedOnEthereumBatch(ctx context.Context) error
+
+	GetAndStoreActionIDForProposeTransferOnElrond(ctx context.Context) (uint64, error)
+	GetAndStoreActionIDForProposeSetStatusFromElrond(ctx context.Context) (uint64, error)
+	GetStoredActionID() uint64
+
+	WasTransferProposedOnElrond(ctx context.Context) (bool, error)
+	ProposeTransferOnElrond(ctx context.Context) error
+
+	WasSetStatusProposedOnElrond(ctx context.Context) (bool, error)
+	ProposeSetStatusOnElrond(ctx context.Context) error
+
+	WasActionSignedOnElrond(ctx context.Context) (bool, error)
+	SignActionOnElrond(ctx context.Context) error
+
+	IsQuorumReachedOnElrond(ctx context.Context) (bool, error)
+	WasActionPerformedOnElrond(ctx context.Context) (bool, error)
+	PerformActionOnElrond(ctx context.Context) error
+	ResolveNewDepositsStatuses(numDeposits uint64)
+
+	ProcessMaxRetriesOnElrond() bool
+	ResetRetriesCountOnElrond()
+
+	MyTurnAsLeaderOnEthereum() bool
+	GetAndStoreBatchFromEthereum(ctx context.Context, nonce uint64) error
+	WasTransferPerformedOnEthereum(ctx context.Context) (bool, error)
+	SignTransferOnEthereum(ctx context.Context) error
+	PerformTransferOnEthereum(ctx context.Context) error
+	IsQuorumReachedOnEthereum(ctx context.Context) (bool, error)
+	WaitForTransferConfirmation(ctx context.Context)
+	GetBatchStatusesFromEthereum(ctx context.Context) ([]byte, error)
+
+	ProcessMaxRetriesOnEthereum() bool
+	ResetRetriesCountOnEthereum()
+
+	IsInterfaceNil() bool
+}
