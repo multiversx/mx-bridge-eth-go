3b49e37852cc0f90135856ab1bcb01d6eb611802
Rebegea Dragos-Alexandru <dragos.rebegea@elrond.com>
2023-02-22 13:30:58 +0200

    add polygon as supported chain

--- clients/batchValidator/batchValidator.go
+++ clients/batchValidator/batchValidator.go
@@ -51,12 +51,12 @@ func NewBatchValidator(args ArgsBatchValidator) (*batchValidator, error) {
 
 func checkArgs(args ArgsBatchValidator) error {
 	switch args.SourceChain {
-	case chain.Ethereum, chain.Bsc, chain.MultiversX:
+	case chain.Ethereum, chain.Bsc, chain.Polygon, chain.MultiversX:
 	default:
 		return fmt.Errorf("%w: %q", clients.ErrInvalidValue, args.SourceChain)
 	}
 	switch args.DestinationChain {
-	case chain.Ethereum, chain.Bsc, chain.MultiversX:
+	case chain.Ethereum, chain.Bsc, chain.Polygon, chain.MultiversX:
 	default:
 		return fmt.Errorf("%w: %q", clients.ErrInvalidValue, args.DestinationChain)
 	}
