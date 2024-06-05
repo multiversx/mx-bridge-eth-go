49b41a23af2c62e7484d2f64efbd012f1e8dda06
Iulian Pascalau <iulian.pascalau@gmail.com>
2024-06-05 10:37:56 +0300

    - integrated new chain simulator through docker image - adapted chain simulator test to run without delays - added slow-tests workflow file

--- integrationTests/interface.go
+++ integrationTests/interface.go
@@ -1,13 +1,9 @@
 package integrationTests
 
-import (
-	"github.com/multiversx/mx-chain-go/node/chainSimulator/dtos"
-	"github.com/multiversx/mx-chain-go/node/chainSimulator/process"
-)
+import "context"
 
-// chainSimulatorHandler defines what a chain simulator should be able to do
-type chainSimulatorHandler interface {
-	GetNodeHandler(shardID uint32) process.NodeHandler
-	SetStateMultiple(stateSlice []*dtos.AddressState) error
+type httpClientWrapper interface {
+	GetHTTP(ctx context.Context, endpoint string) ([]byte, int, error)
+	PostHTTP(ctx context.Context, endpoint string, data []byte) ([]byte, int, error)
 	IsInterfaceNil() bool
 }
