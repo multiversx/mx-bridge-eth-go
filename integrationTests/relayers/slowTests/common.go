7542404fa450d3d27f259c19374fd34203f8afbb
cosmatudor <tudor.cosma53@gmail.com>
2024-12-03 15:44:45 +0200

    fixes after merge

--- integrationTests/relayers/slowTests/common.go
+++ integrationTests/relayers/slowTests/common.go
@@ -3,6 +3,7 @@
 package slowTests
 
 import (
+	"bytes"
 	"math/big"
 
 	bridgeCore "github.com/multiversx/mx-bridge-eth-go/core"
@@ -83,7 +84,7 @@ func GenerateTestUSDCToken() framework.TestTokenParams {
 				},
 				framework.CalledTestSC: {
 					OnEth:    big.NewInt(0),
-					OnMvx:    big.NewInt(0),
+					OnMvx:    big.NewInt(1000),
 					MvxToken: framework.UniversalToken,
 				},
 			},
@@ -345,9 +346,9 @@ func ApplyEUROCRefundBalances(token *framework.TestTokenParams) {
 	// called test SC will have 0 balance since eth->mvx transfer failed
 	token.DeltaBalances[framework.FirstHalfBridge][framework.CalledTestSC].OnMvx = big.NewInt(0)
 	// extra is just for the fees for the 2 transfers mvx->eth and the failed eth->mvx that needed refund
-	token.DeltaBalances[framework.SecondHalfBridge][framework.SafeSC].OnMvx = big.NewInt(50 + 50 + 50)
+	token.DeltaBalances[framework.SecondHalfBridge][framework.SafeSC].OnMvx = big.NewInt(50 + 50 + 50 + 50)
 	// Alice will get her tokens back from the refund
-	token.DeltaBalances[framework.SecondHalfBridge][framework.Alice].OnEth = big.NewInt(-5010 - 7010 - 1010 + 960)
+	token.DeltaBalances[framework.SecondHalfBridge][framework.Alice].OnEth = big.NewInt(-5010 - 7010 - 1010 - 700 + 960 + 650)
 	// no funds remain in the called test SC
 	token.DeltaBalances[framework.SecondHalfBridge][framework.CalledTestSC].OnMvx = big.NewInt(0)
 }
--- integrationTests/relayers/slowTests/edgeCases_test.go
+++ integrationTests/relayers/slowTests/edgeCases_test.go
@@ -1,3 +1,5 @@
+//go:build slow
+
 package slowTests
 
 import (
