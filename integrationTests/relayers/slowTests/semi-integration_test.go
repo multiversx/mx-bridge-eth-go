22610d025ade7370f80cd0641abecae5b622079d
cosmatudor <tudor.cosma53@gmail.com>
2025-01-28 11:48:34 +0200

    added new semi-integration test file

--- integrationTests/relayers/slowTests/semi-integration_test.go
+++ integrationTests/relayers/slowTests/semi-integration_test.go
@@ -5,20 +5,31 @@ package slowTests
 import (
 	"bytes"
 	"context"
-	"github.com/multiversx/mx-bridge-eth-go/integrationTests/relayers/slowTests/framework"
-	"github.com/stretchr/testify/require"
+	"encoding/binary"
 	"math/big"
 	"os"
 	"path"
 	"testing"
+
+	bridgeCore "github.com/multiversx/mx-bridge-eth-go/core"
+	"github.com/multiversx/mx-bridge-eth-go/integrationTests/relayers/slowTests/framework"
+	"github.com/stretchr/testify/require"
+)
+
+const (
+	numRelayers = 3
+	numOracles  = 3
+	quorum      = "03"
+	scAddress   = "erd1qqqqqqqqqqqqqpgqvc7gdl0p4s97guh498wgz75k8sav6sjfjlwqh679jy"
 )
 
 type BridgeProxyTestSetup struct {
 	testing.TB
+	framework.TokensRegistry
 	*framework.KeysStore
-	MultiversxHandler *framework.MultiversxHandler
-	WorkingDir        string
-	ChainSimulator    framework.ChainSimulatorWrapper
+	multiversxHandler *framework.MultiversxHandler
+	workingDir        string
+	chainSimulator    framework.ChainSimulatorWrapper
 
 	ctxCancel func()
 	Ctx       context.Context
@@ -26,18 +37,19 @@ type BridgeProxyTestSetup struct {
 
 func NewSetup(tb testing.TB) *BridgeProxyTestSetup {
 	setup := &BridgeProxyTestSetup{
-		TB:         tb,
-		WorkingDir: tb.TempDir(),
+		TB:             tb,
+		TokensRegistry: framework.NewTokenRegistry(tb),
+		workingDir:     tb.TempDir(),
 	}
-	setup.KeysStore = framework.NewKeysStore(tb, setup.WorkingDir, 3, 3)
+	setup.KeysStore = framework.NewKeysStore(tb, setup.workingDir, numRelayers, numOracles)
 
 	setup.Ctx, setup.ctxCancel = context.WithCancel(context.Background())
 
 	setup.createChainSimulatorWrapper()
 
-	setup.MultiversxHandler = framework.NewMultiversxHandler(setup.TB, setup.Ctx, setup.KeysStore, framework.NewTokenRegistry(tb), setup.ChainSimulator, "03")
+	setup.multiversxHandler = framework.NewMultiversxHandler(setup.TB, setup.Ctx, setup.KeysStore, setup.TokensRegistry, setup.chainSimulator, quorum)
 
-	setup.deployContracts()
+	setup.deployAndSetContracts()
 
 	return setup
 }
@@ -54,52 +66,79 @@ func (setup *BridgeProxyTestSetup) createChainSimulatorWrapper() {
 		ProxyCacherExpirationSeconds: 600,
 		ProxyMaxNoncesDelta:          7,
 	}
-	setup.ChainSimulator = framework.CreateChainSimulatorWrapper(args)
+	setup.chainSimulator = framework.CreateChainSimulatorWrapper(args)
 	require.NoError(setup, err)
 }
 
+func (setup *BridgeProxyTestSetup) deployAndSetContracts() {
+	setup.deployContracts()
+	setup.multiversxHandler.ChangeOwnerForBridgeProxy(setup.Ctx)
+	setup.multiversxHandler.UnpauseBridgeProxy(setup.Ctx)
+	setup.multiversxHandler.SetBridgeProxyAddressOnHelper(setup.Ctx)
+}
+
 func (setup *BridgeProxyTestSetup) deployContracts() {
-	setup.MultiversxHandler.DeployBridgeProxy(setup.Ctx)
+	setup.multiversxHandler.DeployBridgeProxy(setup.Ctx)
 
-	setup.MultiversxHandler.DeployTestHelperContract(setup.Ctx)
+	setup.multiversxHandler.DeployTestHelperContract(setup.Ctx)
 
-	// change MultiTransfer address in MultiSig to be the helper contract
-	setup.MultiversxHandler.MultiTransferAddress = setup.MultiversxHandler.TestHelperAddress
-	setup.MultiversxHandler.SafeAddress = framework.NewMvxAddressFromBech32(setup.TB, "erd1qqqqqqqqqqqqqpgqvc7gdl0p4s97guh498wgz75k8sav6sjfjlwqh679jy")
-	setup.MultiversxHandler.ScProxyAddress = framework.NewMvxAddressFromBech32(setup.TB, "erd1qqqqqqqqqqqqqpgqvc7gdl0p4s97guh498wgz75k8sav6sjfjlwqh679jy")
-	setup.MultiversxHandler.WrapperAddress = framework.NewMvxAddressFromBech32(setup.TB, "erd1qqqqqqqqqqqqqpgqvc7gdl0p4s97guh498wgz75k8sav6sjfjlwqh679jy")
-	setup.MultiversxHandler.AggregatorAddress = framework.NewMvxAddressFromBech32(setup.TB, "erd1qqqqqqqqqqqqqpgqvc7gdl0p4s97guh498wgz75k8sav6sjfjlwqh679jy")
+	setup.multiversxHandler.MultiTransferAddress = setup.multiversxHandler.TestHelperAddress
+	setup.multiversxHandler.SafeAddress = framework.NewMvxAddressFromBech32(setup.TB, scAddress)
+	setup.multiversxHandler.WrapperAddress = framework.NewMvxAddressFromBech32(setup.TB, scAddress)
+	setup.multiversxHandler.AggregatorAddress = framework.NewMvxAddressFromBech32(setup.TB, scAddress)
+	setup.multiversxHandler.DeployMultisig(setup.Ctx)
 
-	setup.MultiversxHandler.DeployMultisig(setup.Ctx)
+	setup.multiversxHandler.DeployTestCaller(setup.Ctx)
+}
+
+func (setup *BridgeProxyTestSetup) generateAndIssueToken() framework.TestTokenParams {
+	token := GenerateUnlistedTokenFromMvx()
+	setup.AddToken(token.IssueTokenParams)
+	setup.multiversxHandler.IssueAndWhitelistToken(setup.Ctx, token.IssueTokenParams)
+
+	return token
 }
 
 func TestBridgeProxy(t *testing.T) {
 	setup := NewSetup(t)
 
-	// send deposit transactions to bridge proxy
+	token := setup.generateAndIssueToken()
+
 	ethTx1 := framework.EthTransaction{
 		From:     bytes.Repeat([]byte{0x01}, 20),
-		To:       bytes.Repeat([]byte{0x02}, 32),
-		TokenID:  "EUROC-123456",
+		To:       setup.multiversxHandler.CalleeScAddress.AddressBytes(),
+		TokenID:  token.MvxUniversalTokenTicker,
 		Amount:   big.NewInt(100),
 		Nonce:    1,
-		CallData: nil,
+		CallData: prependLenAndDataMarker(createScCallData("callPayable", 500000000)),
 	}
 
-	// Define the second EthTransaction
 	ethTx2 := framework.EthTransaction{
-		From:     bytes.Repeat([]byte{0x03}, 20),
-		To:       bytes.Repeat([]byte{0x04}, 32),
-		TokenID:  "USDC-654321",
+		From:     bytes.Repeat([]byte{0x02}, 20),
+		To:       setup.multiversxHandler.CalleeScAddress.AddressBytes(),
+		TokenID:  token.MvxUniversalTokenTicker,
 		Amount:   big.NewInt(2000),
 		Nonce:    2,
-		CallData: nil,
+		CallData: prependLenAndDataMarker(createScCallData("callPayable", 500000000)),
 	}
 
-	// set bridge-proxy address in helper
-	setup.MultiversxHandler.SetBridgeProxyAddressOnHelper(setup.Ctx)
+	// deposit txs in bridge proxy
+	tokenData := setup.TokensRegistry.GetTokenData(token.AbstractTokenIdentifier)
+	setup.multiversxHandler.CallDepositOnBridgeProxy(setup.Ctx, ethTx1, 0, tokenData)
+	setup.multiversxHandler.CallDepositOnBridgeProxy(setup.Ctx, ethTx2, 0, tokenData)
 
-	// create Deposit Tx in handler
-	setup.MultiversxHandler.CallDepositOnBridgeProxy(setup.Ctx, ethTx1, 0)
-	setup.MultiversxHandler.CallDepositOnBridgeProxy(setup.Ctx, ethTx2, 0)
+	// make 2 execute calls for same deposit
+	setup.multiversxHandler.ExecuteDepositWithoutGenerateBlocks(setup.Ctx, 1, 0)
+	setup.multiversxHandler.ExecuteDepositWithoutGenerateBlocks(setup.Ctx, 1, 1)
+
+	setup.chainSimulator.GenerateBlocks(setup.Ctx, 10)
+}
+
+func prependLenAndDataMarker(input []byte) []byte {
+	buff32 := make([]byte, bridgeCore.Uint32ArgBytes)
+	binary.BigEndian.PutUint32(buff32, uint32(len(input)))
+
+	prefix := append([]byte{bridgeCore.DataPresentProtocolMarker}, buff32...)
+
+	return append(prefix, input...)
 }
