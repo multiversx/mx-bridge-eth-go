e3c7b1d640105c46c83765230d30a5df198d0c0d
cosmatudor <tudor.cosma53@gmail.com>
2025-08-29 11:45:21 +0300

    debug log

--- factory/suiMvxBridgeComponents.go
+++ factory/suiMvxBridgeComponents.go
@@ -87,57 +87,59 @@ func NewSuiMvxBridgeComponents(args ArgsSuiToMultiversXBridge) (*suiMvxBridgeCom
 		suiApi:               args.SuiProxy,
 	}
 
+	log := logger.GetOrCreate("debug")
+
 	err = components.initBaseComponents(commonBridgeArgs)
 	if err != nil {
-		fmt.Println("Error initializing base components:", err)
+		log.Debug("Error initializing base components:", err)
 		return nil, err
 	}
 
 	err = components.createSuiKeysAndAddresses(args.Configs.GeneralConfig.Sui)
 	if err != nil {
-		fmt.Println("Error creating Sui keys and addresses:", err)
+		log.Debug("Error creating Sui keys and addresses:", err)
 		return nil, err
 	}
 
 	err = components.createSuiDataGetter(args)
 	if err != nil {
-		fmt.Println("Error creating Sui data getter:", err)
+		log.Debug("Error creating Sui data getter:", err)
 		return nil, err
 	}
 
 	err = components.createSuiRoleProvider(args)
 	if err != nil {
-		fmt.Println("Error creating Sui role provider:", err)
+		log.Debug("Error creating Sui role provider:", err)
 		return nil, err
 	}
 
 	err = components.createSuiClient(args)
 	if err != nil {
-		fmt.Println("Error creating Sui client:", err)
+		log.Debug("Error creating Sui client:", err)
 		return nil, err
 	}
 
 	err = components.createSuiToMultiversXBridge(args)
 	if err != nil {
-		fmt.Println("Error creating Sui to MultiversX bridge:", err)
+		log.Debug("Error creating Sui to MultiversX bridge:", err)
 		return nil, err
 	}
 
 	err = components.createSuiToMultiversXStateMachine()
 	if err != nil {
-		fmt.Println("Error creating Sui to MultiversX state machine:", err)
+		log.Debug("Error creating Sui to MultiversX state machine:", err)
 		return nil, err
 	}
 
 	err = components.createMultiversXToSuiBridge(args)
 	if err != nil {
-		fmt.Println("Error creating MultiversX to Sui bridge:", err)
+		log.Debug("Error creating MultiversX to Sui bridge:", err)
 		return nil, err
 	}
 
 	err = components.createMultiversXToSuiStateMachine()
 	if err != nil {
-		fmt.Println("Error creating MultiversX to Sui state machine:", err)
+		log.Debug("Error creating MultiversX to Sui state machine:", err)
 		return nil, err
 	}
 
